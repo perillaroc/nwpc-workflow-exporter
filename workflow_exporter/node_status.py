7a89127b594f8e7a1e889754eb60becb0c58d851
perillaroc <perillaroc@gmail.com>
2019-11-16 20:58:30 +0800

    :loud_sound: add more logs.

--- workflow_exporter/node_status.py
+++ workflow_exporter/node_status.py
@@ -1,24 +1,34 @@
 # coding: utf-8
+import time
+import json
+import logging
+
 import click
 import yaml
 import redis
-import time
-import json
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 @click.command("node-status")
 @click.option("--config-file", help="config file path", required=True)
 def node_status(config_file):
+    logger = logging.getLogger("node-status")
     with open(config_file) as f:
         config = yaml.safe_load(f)
 
     redis_config = config["global"]["redis"]
 
+    redis_host = redis_config["host"]
+    redis_port = redis_config["port"]
+    redis_password = redis_config["password"]
+    redis_db = redis_config["db"]
+    logger.info(f"connecting to redis...{redis_host}:{redis_port}/{redis_db}")
     redis_client = redis.Redis(
-        host=redis_config["host"],
-        port=redis_config["port"],
-        password=redis_config["password"],
-        db=redis_config["db"]
+        host=redis_host,
+        port=redis_port,
+        password=redis_password,
+        db=redis_db
     )
 
     owner = config["global"]["owner"]
@@ -30,7 +40,10 @@ def node_status(config_file):
 
     while True:
         message = p.get_message()
-        if message and message["type"] == "message":
-            data = json.loads(message["data"])
-            print(data["collected_time"])
+        if message:
+            if message["type"] == "message":
+                data = json.loads(message["data"])
+                logger.info(data["collected_time"])
+            else:
+                logger.info("type is not message: {message}".format(message=message))
         time.sleep(1)
